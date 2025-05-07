b189ed5f22e66cf69aa874d27654cfb92ab5ff31
Tsubasa Kurashina <12211026@mail.sustech.edu.cn>
2025-05-07 22:04:13 +0800

    试图建立ai语音识别

--- app/src/test/java/com/example/pa/ExampleUnitTest.java
+++ app/src/test/java/com/example/pa/ExampleUnitTest.java
@@ -30,6 +30,7 @@ public class ExampleUnitTest {
     public void test_hash_password() throws NoSuchAlgorithmException {
         assertEquals("2f75ad19416b54cdf23bf37d58650b4414ce75a6a3694b18ad703ef682775cef", sha256("ghj"));
         assertEquals("1a946a41ee4d54d8af5c2044a9fbb0adab51c44be38759bc5fd7d5083b61e3af", sha256("7hua931"));
+        assertEquals("558e34106b6d0f742378c2c03b9f7df1f2721aa7f73062ff3cbd5c9083533f44", sha256("你其实是傻逼"));
     }
 
 }
