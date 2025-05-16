aa99d1d274d3c00330a6889175ee0a7b9b4c5fdf
zbc <203830839@qq.com>
2025-05-16 16:30:46 +0800

    重构SocialFragment类，使软件不会崩溃

--- app/src/main/java/com/example/pa/ui/social/SocialFragment.java
+++ app/src/main/java/com/example/pa/ui/social/SocialFragment.java
@@ -1,4 +1,26 @@
 package com.example.pa.ui.social;
 
-public class SocialFragment {
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pa.R;
+
+public class SocialFragment extends Fragment {
+
+    public SocialFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_social, container, false);
+    }
 }
